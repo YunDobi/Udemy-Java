2cd5c5bb71f242e897b70f83ea1f53a787061533
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-01-16 18:00:47 -0800

    Inheritance

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    private String make;
+    private String color;
+    private String model;
+
+    public void detailCar () {
+        System.out.println("the car has " + color + " and " + model);
+    }
+}
