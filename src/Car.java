7622afb185a62d83e9ce022d6415544fffb7789f
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-01-25 14:52:29 -0800

    work

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,18 @@
 public class Car {
-    private String make;
-    private String color;
-    private String model;
+    private String make = "makes";
+    private String color = "White";
+    private String model = "T";
+    public String getMake() {
+        return make;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
 
     public void detailCar () {
         System.out.println("the car has " + color + " and " + model);
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,9 @@
 //overloading paractice
 public class Main {
     public static void main(String[] args) {
-        int count = 0;
-        int sum = 0;
-
-        for (int i = 1;  i <= 1000; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                count += 1;
-                sum += i;
-                System.out.println(i);
-            }
-            if (count == 5) {
-                break;
-            }
-        }
-        System.out.println(sum);
+        Car car = new Car();
+        System.out.println(car.getColor());
+        car.detailCar();
     }
 //    public static int calculate (String playerName, int score) {
 //        System.out.println("player " + playerName + " score " + score);
