66f437f62d64e10fd5ed29ec8cb9eb8d67b802ee
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-01-31 15:46:08 -0800

    Jan 31 for getter and setter

---
+++ src/Customer.java
@@ -0,0 +1,29 @@
+public class Customer {
+    private String name;
+    private double creatLimit;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCreatLimit() {
+        return creatLimit;
+    }
+
+    public void setCreatLimit(double creatLimit) {
+        this.creatLimit = creatLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
